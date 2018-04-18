cd62a6c6b7bec0754b5f054f58556d6d71d7614b
Keith McDuffee <gudlyf@realistek.com>
2018-04-18 07:44:07 -0400

    Initial script and early README info.

---
+++ KidsScreenTimeLocker.ps1
@@ -0,0 +1,68 @@
+# Helper functions for building the class
+$script:nativeMethods = @();
+function Register-NativeMethod([string]$dll, [string]$methodSignature)
+{
+$script:nativeMethods += [PSCustomObject]@{ Dll = $dll; Signature = $methodSignature; }
+}
+function Add-NativeMethods()
+{
+$nativeMethodsCode = $script:nativeMethods | % { "
+[DllImport(`"$($_.Dll)`")]
+public static extern $($_.Signature);
+" }
+
+Add-Type @"
+using System;
+using System.Runtime.InteropServices;
+public static class NativeMethods {
+$nativeMethodsCode
+}
+"@
+} 
+
+Register-NativeMethod "user32.dll" "bool LockWorkStation()"
+Add-NativeMethods
+
+# Prompt for time
+
+# Dialog prompt
+[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
+$intReturn = $timertimemins = [Microsoft.VisualBasic.Interaction]::InputBox("Enter time in minutes", "Timer Time", 60)
+$timertime = ([int]$timertimemins * 60)
+
+# Timer Popup
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+$Form = New-Object System.Windows.Forms.Form
+$Font = New-Object System.Drawing.Font("BadaBoom BB",18,[System.Drawing.FontStyle]::Regular)
+$Form.Font = $Font
+$Form.Width = 350
+$Form.Height = 80
+$script:Label = New-Object System.Windows.Forms.Label
+$script:Label.AutoSize = $true
+$script:Form.Controls.Add($Label)
+$Timer = New-Object System.Windows.Forms.Timer
+$Timer.Interval = 1000
+$script:CountDown = $timertime
+$Timer.add_Tick(
+    {
+        $CountDownMins = [math]::Round($CountDown / 60)
+        If($CountDown -lt 120) {
+           $script:Label.Text = "Your system will lock in $CountDown seconds"
+        }
+        Else {
+          $script:Label.Text = "Your system will lock in $CountDownMins minutes"
+        }
+        $script:CountDown--
+        If($CountDown -lt 300) {
+          $Form.BackColor = "Red"
+        }
+        If($CountDown -lt 0) {
+          $script:Timer.Stop()
+          $script:Form.Close()
+        }
+    }
+)
+$script:Timer.Start()
+$script:Form.ShowDialog()
+[NativeMethods]::LockWorkStation()
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # KidsScreenTimeLocker
+
+- Place KidsScreenTimeLocker in `C:\`
+
+- Create shortcut to KidscreenTime PS1 script.
+
+- Right-click shortcut and select "Properties."
+
+- Edit "Target" field with the following:
+
+`C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -WindowStyle hidden -File "C:\KidsScreenTimeLocker.ps1"`
+
+- To run the lock script, double-click the shortcut and set the time in minutes before screen lock.
+
+- Closing the timer window will immediately lock the computer.
+
+- You may want to disable the ability to use Task Manager, as any tech-savvy kid can find and kill the PowerShell process and end the timer.
